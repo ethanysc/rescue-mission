2d6327e5644e810ba7fe10b621a0a03938a33f81
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-07-16 14:10:22 -0400

    question show page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,8 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all.order(created_at: :asc)
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
