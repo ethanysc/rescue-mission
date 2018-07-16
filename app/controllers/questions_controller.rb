1b7d441277492f93f479b0e3f4e871b5b13cefee
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-07-16 14:41:41 -0400

    Post a new question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,28 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: "You succesfully submitted a Question you Jabroni!"
+    else
+      @errors = @question.errors.full_messages.join(', ')
+      render action: 'new'
+    end
+  end
+
+
+
+  private
+
+  def question_params
+   params.require(:question).permit(:title, :description)
+ end
 end
