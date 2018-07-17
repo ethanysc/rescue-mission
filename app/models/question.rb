5ff93ff61bf5008e1caf0e9c3057181628fe7936
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-07-17 15:08:05 -0400

    delete question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: { minimum: 20 }
   validates :description, presence: true, length: { minimum: 50 }
 
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'questions#index'
   devise_for :users
 
-  resources :questions, only: [:index, :new, :create, :edit, :update] do
+  resources :questions, only: [:index, :new, :create, :edit, :update, :destroy] do
     resources :answers, only: [:new, :create]
   end
 
